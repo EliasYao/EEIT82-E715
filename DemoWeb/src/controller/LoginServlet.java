0b62148509bfffdf37457d54070a8e294b98e9a0
eliasyao <eliasyaoyj@gmail.com>
2015-11-30 15:23:23 +0800

    asdasdasd

--- DemoWeb/src/controller/LoginServlet.java
+++ DemoWeb/src/controller/LoginServlet.java
@@ -22,7 +22,7 @@ public class LoginServlet extends HttpServlet {
 //接收資料
 		String username = request.getParameter("username");
 		String password = request.getParameter("password");
-		System.out.println("789");
+		System.out.println("aaaaaaaaaaa");
 //驗證資料
 		Map<String, String> errors = new HashMap<String, String>();
 		request.setAttribute("errors", errors);
